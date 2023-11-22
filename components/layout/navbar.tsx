726f0d84cc8b1bad9f8e64ad162f298e8b27f459
docu.run <docudotrun@gmail.com>
2023-11-22 08:47:48 +0700

    remove login button

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -43,21 +43,7 @@ export function NavBar({ user, items, children, rightElements, scroll = false }:
               >
                 Login Page
               </Link>
-            ) : null}
-
-            {user ? (
-              <UserAccountNav user={user} />
-            ) : (
-              <Link
-                href="/login"
-                className={cn(
-                  buttonVariants({ variant: "ghost" }),
-                  "absolute right-4 top-4 md:right-8 md:top-8"
-                )}
-              >
-                Login
-              </Link>
-            )}
+            ) : <UserAccountNav user={user} />}
           </div>
         </div>
       </header>
