085ea73d078e0bf6f8cd08c4bb8c5c250ddcc6c9
Son Dinh <99938067+restaurant365-sdinh@users.noreply.github.com>
2023-11-23 18:46:55 +0700

    update upload

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link"
-import { useState } from "react";
+import React, { useState } from "react";
 import { buttonVariants } from "@/components/ui/button"
 import { siteConfig } from "@/config/site"
 import { cn, nFormatter } from "@/lib/utils"
@@ -10,31 +10,47 @@ import { Icons } from "@/components/shared/icons"
 import { env } from "@/env.mjs"
 
 export default async function IndexPage(props) {
-  const [image, setImage] = useState(null);
-  const [createObjectURL, setCreateObjectURL] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+    const inputFileRef = React.useRef<HTMLInputElement | null>(null);
 
-  const uploadToClient = (event) => {
-    if (event.target.files && event.target.files[0]) {
-      const i = event.target.files[0];
+    const handleOnClick = async (e: React.MouseEvent<HTMLInputElement>) => {
 
-      setImage(i);
-      setCreateObjectURL(URL.createObjectURL(i));
-    }
-  };
+        /* Prevent form from submitting by default */
+        e.preventDefault();
 
-  const uploadToServer = async (event) => {
-    const body = new FormData();
-    if(image){
-      body.append("file", image);
-      const response = await fetch("/api/file", {
-        method: "POST",
-        body
-      });
-    }
-    else{
-      console.log("The image is null.")
-    }
-  };
+        /* If file is not selected, then show alert message */
+        if (!inputFileRef.current?.files?.length) {
+            alert('Please, select file you want to upload');
+            return;
+        }
+
+        setIsLoading(true);
+
+        /* Add files to FormData */
+        const formData = new FormData();
+        Object.values(inputFileRef.current.files).forEach(file => {
+            formData.append('file', file);
+        })
+
+        /* Send request to our api route */
+        const response = await fetch('/api/upload', {
+            method: 'POST',
+            body: formData
+        });
+
+        const body = await response.json() as { status: 'ok' | 'fail', message: string };
+
+        alert(body.message);
+
+        if (body.status === 'ok') {
+            inputFileRef.current.value = '';
+            // Do some stuff on successfully upload
+        } else {
+            // Do some stuff on error
+        }
+
+        setIsLoading(false);
+    };
 
 
   return (
@@ -72,20 +88,15 @@ export default async function IndexPage(props) {
           </p>
           <p>
             <Balancer>
-              <div>
+              <form>
                 <div>
-                  <img src={createObjectURL} />
-                  <h4>Select Image</h4>
-                  <input type="file" name="myImage" onChange={uploadToClient} />
-                  <button
-                    className="btn btn-primary"
-                    type="submit"
-                    onClick={uploadToServer}
-                  >
-                    Send to server
-                  </button>
+                    <input type="file" name="myfile" ref={inputFileRef} multiple />
                 </div>
-              </div>
+                <div>
+                    <input type="submit" value="Upload" disabled={isLoading} onClick={handleOnClick} />
+                    {isLoading && ` Wait, please...`}
+                </div>
+              </form>
             </Balancer>
           </p>
           <div
