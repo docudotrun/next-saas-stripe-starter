652709ac45de68903ba7af05a6fb0e44f8d4d161
Son Dinh <99938067+restaurant365-sdinh@users.noreply.github.com>
2023-11-24 13:53:56 +0700

    fix missing filename

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -6,16 +6,22 @@ export async function POST(request: Request): Promise<NextResponse> {
   const filename = searchParams.get('filename');
 
   // ⚠️ The below code is for App Router Route Handlers only
-  const blob = await put(filename, request.body, {
-    access: 'public',
-  });
+  if(filename){
+    const blob = await put(filename, request.body, {
+      access: 'public',
+    });
+    // Here's the code for Pages API Routes:
+    // const blob = await put(filename, request, {
+    //   access: 'public',
+    // });
 
-  // Here's the code for Pages API Routes:
-  // const blob = await put(filename, request, {
-  //   access: 'public',
-  // });
-
-  return NextResponse.json(blob);
+    return NextResponse.json(blob);
+  }
+  else{
+    const message = "The filename must not be empty.";
+    const response = new NextResponse(message, { status: 400 });
+    return response.send(response);
+  }
 }
 
 // The next lines are required for Pages API Routes only
