307d7dd6f43a7a4b0ae194be4f0f41765f0c4793
Son Dinh <99938067+restaurant365-sdinh@users.noreply.github.com>
2023-11-24 14:01:45 +0700

    use pages API routes

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -7,13 +7,13 @@ export async function POST(request: Request): Promise<NextResponse> {
 
   // ⚠️ The below code is for App Router Route Handlers only
   if(filename){
-    const blob = await put(filename, request.body, {
+    //const blob = await put(filename, request.body, {
+    //  access: 'public',
+    //});
+    // Here's the code for Pages API Routes:
+    const blob = await put(filename, request, {
       access: 'public',
     });
-    // Here's the code for Pages API Routes:
-    // const blob = await put(filename, request, {
-    //   access: 'public',
-    // });
 
     return NextResponse.json(blob);
   }
@@ -25,8 +25,8 @@ export async function POST(request: Request): Promise<NextResponse> {
 }
 
 // The next lines are required for Pages API Routes only
-// export const config = {
-//   api: {
-//     bodyParser: false,
-//   },
-// };
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
