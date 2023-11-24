407b77b4e9f03bf6996f599164a19fe82ea0603a
Son Dinh <99938067+restaurant365-sdinh@users.noreply.github.com>
2023-11-24 14:24:20 +0700

    update the upload component

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,25 +1,21 @@
-import { put } from '@vercel/blob';
-import { NextResponse } from 'next/server';
+import { put } from '@vercel/blob'
+import { NextResponse } from 'next/server'
+import { customAlphabet } from 'nanoid'
 
-export async function POST(request: Request): Promise<NextResponse> {
-  const { searchParams } = new URL(request.url);
-  const filename = searchParams.get('filename');
+export const runtime = 'edge'
 
-  // ⚠️ The below code is for App Router Route Handlers only
-  if(filename){
-    //const blob = await put(filename, request.body, {
-    //  access: 'public',
-    //});
-    // Here's the code for Pages API Routes:
-    const blob = await put(filename, request, {
-      access: 'public',
-    });
+const nanoid = customAlphabet(
+  '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
+  7
+) // 7-character random string
+export async function POST(req: Request) {
+  const file = req.body || ''
+  const contentType = req.headers.get('content-type') || 'text/plain'
+  const filename = `${nanoid()}.${contentType.split('/')[1]}`
+  const blob = await put(filename, file, {
+    contentType,
+    access: 'public',
+  })
 
-    return NextResponse.json(blob);
-  }
-  else{
-    const message = "The filename must not be empty.";
-    const response = new NextResponse(message, { status: 400 });
-    return response.send(response);
-  }
+  return NextResponse.json(blob)
 }
